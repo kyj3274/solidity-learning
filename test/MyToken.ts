3907ca77d9d33c0e0e7a1f7e396d94bfdf13e51b
kyj3274 <hjkl3274@gmail.com>
2025-04-13 17:10:35 +0900

    signer1번에 의한 signer0번 자산 이동 (approve & transferFrom) 과제 - ts

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -53,4 +53,57 @@ describe("My Token", () => {
             ).to.be.revertedWith("insufficient balance");
         });
     });
+
+    describe("TransferFrom", () => { 
+        it("should emit Approval event", async () => {
+            const signer1 = signers[1];
+            await expect(
+                myTokenC.approve(signer1.address, hre.ethers.parseUnits("10", decimals))
+            )
+                .to.emit(myTokenC, "Approval")
+                .withArgs(signer1.address, hre.ethers.parseUnits("10", decimals));
+        });
+        it("should be reverted with insufficient allowance error", async () => {
+            const signer0 = signers[0];
+            const signer1 = signers[1];
+            await expect(
+                myTokenC
+                .connect(signer1)
+                .transferFrom(
+                    signer0.address, 
+                    signer1.address, 
+                    hre.ethers.parseUnits("1", decimals)
+                )
+            ).to.be.revertedWith("insufficient allowance");
+        });
+    });
+    //     1. approve: signer1에게 signer0의 자산 이동권한 부여
+    //     2. transferFrom: signer1이 signer0의 MT토큰을 자신의 주소(signer1)에게 전송
+    //     3. balance 확인
+    //     TestCase
+    it("should approve and transferFrom and check balance", async () => {
+        const signer0 = signers[0];
+        const signer1 = signers[1];
+        //approve : signer0의 자산 이동 권한 -> signer1
+        await expect(
+            myTokenC
+                .connect(signer0)
+                .approve(signer1.address, hre.ethers.parseUnits("10", decimals))
+        )
+            .to.emit(myTokenC, "Approval")
+            .withArgs(signer1.address, hre.ethers.parseUnits("10", decimals));
+        //transferFrom : signer0의 3MT토큰을 signer1(자신)로 전송
+        await expect(
+            myTokenC
+                .connect(signer1)
+                .transferFrom(
+                    signer0.address,
+                    signer1.address,
+                    hre.ethers.parseUnits("3", decimals) // signer0의 3MT signer1로 전송
+                )
+        ).to.emit(myTokenC, "Transfer").withArgs(signer0.address, signer1.address, hre.ethers.parseUnits("3", decimals));
+        //balanceOf
+        expect(await myTokenC.balanceOf(signer0.address)).to.equal(hre.ethers.parseUnits("97", decimals));
+        expect(await myTokenC.balanceOf(signer1.address)).to.equal(hre.ethers.parseUnits("3", decimals));
+    });
 });
